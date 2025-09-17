713919a0502982ea6597e74ea8136f5d5bd22f5d
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2025-09-17 01:08:18 -0600

    adding project

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func updateBall(){
+        var randomBallIndex:Int = 0
+        randomBallIndex = Int.random(in: 1...5)
+        imageView.image = UIImage(named: "ball\(randomBallIndex)")
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake{
+            updateBall()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateBall()
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        updateBall()
+    }
+    
 }
 
