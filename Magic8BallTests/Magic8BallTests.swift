5d4ad5d3b8667804b8b04afe43978e218beefa7c
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2025-09-17 00:19:26 -0600

    Initial Commit

---
+++ Magic8BallTests/Magic8BallTests.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallTests.swift
+//  Magic8BallTests
+//
+//  Created by Emmanuel Lopez Guerrero on 17/09/25.
+//
+
+import Testing
+@testable import Magic8Ball
+
+struct Magic8BallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Magic8BallUITests/Magic8BallUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Magic8BallUITestsLaunchTests.swift
+//  Magic8BallUITests
+//
+//  Created by Emmanuel Lopez Guerrero on 17/09/25.
+//
+
+import XCTest
+
+final class Magic8BallUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
